844522ce352bec3b816ceb3d5961bda497dade9a
Jake Elsberry <jelsberry96@gmail.com>
2023-10-11 10:24:53 -0700

    connect to DB
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>

--- Parcels/Program.cs
+++ Parcels/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using DoctorsOffice.Models;
+using Parcels.Models;
 
 namespace Parcels
 {
